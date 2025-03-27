f4c3b7bba6c6088df14ede4e42b4bff63e3c46b4
p1ckle-r1ck <p1ckle-r1ck@MacBook-Air-p1ckle-r1ck.local>
2025-03-27 09:55:08 +0300

    git ignore update

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -18,6 +18,7 @@ terraform {
     skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
     skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
 
+
   }
 }
 
