9b7eb562fc002ccb71b36d372602e79ede49e901
p1ckle-r1ck <p1ckle-r1ck@MacBook-Air-p1ckle-r1ck.local>
2025-03-27 09:41:24 +0300

    add structure

---
+++ infrastructure/backend.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+
+  backend "s3" {
+    endpoints = {
+      s3 = "http://storage.yandexcloud.net"
+    }
+    bucket = "marsel.valiev"
+    region = "ru-central1-a"
+    key    = "infrastructure.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+  }
+}
+
+provider "yandex" {
+    folder_id                = var.cloud.folder_id
+    zone                     = var.cloud.default_zone
+    service_account_key_file = file("key.json")
+    cloud_id = var.cloud.cloud_id  
+}
---
+++ infrastructure/k8s_instance.tf
@@ -0,0 +1,51 @@
+resource "yandex_compute_instance" "master_node" {
+    resources {
+      memory = var.k8s-instance.master_node.memory
+      cores = var.k8s-instance.master_node.cores
+      core_fraction = var.k8s-instance.master_node.core_fraction
+    }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.a_subnet.id
+    nat = true
+  }
+  boot_disk {
+    initialize_params {
+      image_id = var.k8s-instance.master_node.image_id
+    }
+  }
+}
+
+resource "yandex_compute_instance" "worker_node_1" {
+    resources {
+      memory = var.k8s-instance.worker_node_1.memory
+      cores = var.k8s-instance.worker_node_1.cores
+      core_fraction = var.k8s-instance.worker_node_1.core_fraction
+    }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.a_subnet.id
+    nat = true
+
+  }
+  boot_disk {
+    initialize_params {
+      image_id = var.k8s-instance.master_node.image_id
+    }
+  }
+}
+
+resource "yandex_compute_instance" "worker_node_2" {
+    resources {
+      memory = var.k8s-instance.worker_node_2.memory
+      cores = var.k8s-instance.worker_node_2.cores
+      core_fraction = var.k8s-instance.worker_node_2.core_fraction
+    }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.a_subnet.id
+    nat =  true
+  }
+  boot_disk {
+    initialize_params {
+      image_id = var.k8s-instance.master_node.image_id
+    }
+  }
+}
